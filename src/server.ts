cdb54f13f4512d7d5ff1654d58110b2960ff9759
Kamrul123 <kamrul.bubtr@gmail.com>
2026-07-17 21:48:21 +0600

    chore: respond to deployment requirements for vercel

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import app from './app';
 
-const PORT = process.env.PORT || 5000;
+if (process.env.VERCEL === undefined) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+export default app;
